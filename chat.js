d37a8ba5946eda17f626d54fa72ead8fcad1714b
macournoyer <macournoyer@gmail.com>
2012-10-23 12:31:01 -0400

    Refactor chat server to use callbacks.

--- chat.js
+++ chat.js
@@ -32,20 +32,28 @@ function disconnect(fd) {
   console.log("User disconnected on FD: " + fd);
   syscalls.close(fd);
   users.splice(users.indexOf(fd), 1);
+  delete callbacks[fd];
+}
+
+var callbacks = {}; // {fd: function() {...}}
+
+callbacks[acceptFd] = function() {
+  var userFd = accept();
+  
+  callbacks[userFd] = function() {
+    // user sent a message
+    readAndBroadcastMessage(userFd);
+  }
 }
 
 while (true) {
-  var fds = syscalls.select(users.concat(acceptFd), [], []);
+  var fds = syscalls.select(Object.keys(callbacks), [], []);
   
   var readableFds = fds[0];
   
   readableFds.forEach(function(fd) {
-    if (fd == acceptFd) {
-      accept();
-    } else {
-      // user sent a message
-      readAndBroadcastMessage(fd);
-    }
+    var callback = callbacks[fd];
+    callback();
   });
 }
 
